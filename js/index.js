e8be75acd0a2b2a31c58f5ff2b672c6e8c8388e4
Fabio Madeira Reges <adv.fabioreges@live.com>
2021-05-20 17:29:21 -0300

    Adicionar arquivos de projeto.

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+var nome = "Fabio Mad. Reges";
+var cargo = "CTO Growdev";
+
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+
+function colocarNomeNoHtml(nome) {
+    nomeHtml.innerHTML = nome;
+
+
+}
+
+
+function colocarcargonohtml(cargo) {
+    cargoHtml.innerHTML = cargo;
+
+}
+
+function clickNoProjetos() {
+    console.log("Clicou no botão projetos!!!");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clickNoSobre() {
+    console.log("Clicou no botão Sobre!!!");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+colocarNomeNoHtml(nome);
+colocarcargonohtml(cargo);
